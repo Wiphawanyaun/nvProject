274c809ee425151f7c622b10a0e620b6b40a527c
Wiphawan Piapramong <gitwiphawan@gmail.com>
2022-09-14 09:55:55 +0700

    commit-9-14

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
-import { View, Text,Button } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-function HomeScreen({navigation}) {
+import { DefaultTheme } from "@react-navigation/native";
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
+
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home!</Text>
-      <Button title="go to settings"
-       onPress={() => navigation.navigate("Setting")}/>
-      
+      <Button
+        title="go to settings"
+        onPress={() => navigation.navigate("Setting")}
+      />
     </View>
   );
 }
 
-function SettingScreen({navigation}) {
+function SettingScreen({ navigation }) {
   return (
-    <View  style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Setting!</Text>
-      <Button title="go to home"
-       onPress={() => navigation.goBack()}/>
+      <Button title="go to home" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -29,18 +41,50 @@ const Tab = createBottomTabNavigator();
 
 function Mytabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Setting") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          }
+          //you can return any component that you like here
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Setting" component={SettingScreen} />
     </Tab.Navigator>
   );
 }
 
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator useLegacyImplementation>
+      <Drawer.Screen name="Home" component={Mytabs} />
+      <Drawer.Screen name="Setting" component={SettingScreen} />
+    </Drawer.Navigator>
+  );
+}
+
 const App = () => {
   return (
-  <NavigationContainer>
-    <Mytabs/>
-  </NavigationContainer>);
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer>
+        <Mytabs />
+      </MyDrawer>
+    </NavigationContainer>
+  );
 };
 
 export default App;
